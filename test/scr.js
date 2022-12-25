d18f6834314a3ac6e49d2280ef2b6dc36f629393
DarkSteelD <101361201+DarkSteelD@users.noreply.github.com>
2022-12-26 00:26:26 +0300

    final-1

--- test/scr.js
+++
@@ -1,9 +0,0 @@
-// Ждем загрузки DOM. Элемент <body> будет загружен автоматически.
-window.addEventListener('DOMContentLoaded', function() {
-  var button = document.getElementById('button');
-  button.addEventListener('click', function(event) {
-    // Переключаем класс .opened
-    event.target.classList.toggle('opened');
-  });
-
-});
